053410ffa0c2130179e03b2f8a95a03cca7b756d
gabrieliromagnarita <gabihromagna@gmail.com>
2025-06-07 14:31:45 -0300

    tela de game over

---
+++ JogoChemApocalypse/Assets/gameOver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameOver : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    public void gameOver
+    {
+        
+    }
+}
