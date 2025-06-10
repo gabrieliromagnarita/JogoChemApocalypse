bbae0cdf6c601b54dabb6f1a313ac946f52766ca
MirelaPedro <mirelapedrotereza@gmail.com>
2025-06-10 18:20:10 -0300

    inimigo - inicio do script

---
+++ JogoChemApocalypse/Assets/Personagens/Zumbi/Script/NaveMesh.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NaveMesh : MonoBehaviour {
+
+    [SerializeField] private NavMeshAgent inimigo;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        inimigo.SetDestination(player.position);
+    }
+}
