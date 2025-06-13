ebb315432ded31bb8646a1c5066aa4190ae16285
MirelaPedro <mirelapedrotereza@gmail.com>
2025-06-13 14:43:22 -0300

    conflitos resolvidos

--- JogoChemApocalypse/Assets/AnimacoesBackup/Personagens/Zoe/Script/ZoeMove.cs
+++ JogoChemApocalypse/Assets/AnimacoesBackup/Personagens/Zoe/Script/ZoeMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class PlayerMove : MonoBehaviour
+public class ZoeMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Referência ao CharacterController da Unity
